3144371f19a9cd1516b1e0c4c4e9e4bd55d70381
plandem <plandem@gmail.com>
2016-02-20 00:52:44 +0300

    ru locale

--- src/locale/ru_RU.js
+++ src/locale/ru_RU.js
@@ -1,11 +1,15 @@
-/*
- * ru-RU (Moscow) locale
- * @ignore
- * @author plandem@gmail.com
- */
 module.exports = {
-	// in minutes
-	timezoneOffset: 3 * 60,
-	firstDayOfWeek: 1,
-	minimalDaysInFirstWeek: 1,
+	eras: ['до н.э.', 'н.э.'],
+	months: ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль',
+		'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+	shortMonths: ['Янв.', 'Фев.', 'Мар.', 'Апр.', 'Май', 'Июн.', 'Июл.', 'Авг.', 'Сен.',
+		'Окт.', 'Ноя.', 'Дек.'],
+	weekdays: ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница',
+		'Суббота'],
+	shortWeekdays: ['Вск', 'Пнд', 'Втр', 'Срд', 'Чтв', 'Птн', 'Сбт'],
+	veryShortWeekdays: ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+	ampms: ['AM', 'PM'],
+	datePatterns: ['EEEE, d MMMM, yyyy', 'd MMMM, yyyy', 'd MMM, yyyy', 'd-M-yy'],
+	timePatterns: ['H:mm:ss \'GMT\'z', 'H:mm:ss', 'H:mm:ss', 'H:mm'],
+	dateTimePattern: '{date} {time}',
 };
