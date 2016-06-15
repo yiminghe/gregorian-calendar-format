32d1f8156f73b2db22ddda1073012e3819a021dd
Diego Leonardo Urban <diego.urban88@gmail.com>
2016-06-14 21:44:02 -0300

    Brazilian portuguese locale

---
+++ src/locale/pt_BR.js
@@ -0,0 +1,12 @@
+module.exports = {
+  eras: ['AC', 'DC'],
+  months: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+  shortMonths: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+  weekdays: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'],
+  shortWeekdays: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  veryShortWeekdays: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+  ampms: ['AM', 'PM'],
+  datePatterns: ['dd/mm/yy', 'dd/mm/yyyy'],
+  timePatterns: ['h:mm:ss a \'GMT\'Z', 'h:mm:ss a', 'h:mm:ss a', 'h:mm a'],
+  dateTimePattern: '{date} {time}',
+};
